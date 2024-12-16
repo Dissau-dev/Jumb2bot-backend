33e615d0f7bc177fb02f2b482e696ec185054a13
Mauricio Garcia <mauriciogaraco@gmail.com>
2024-12-16 15:50:25 -0500

    plan update

--- src/helper/prices.js
+++ src/helper/prices.js
@@ -2,16 +2,19 @@
 // priceId de los planes creados en Stripe
 const PRICES = [{
     "product_name":"Monthly",
+    "price": "$9.99",
     "product_id":"prod_RIU4FwbVdjOke9",
     "price_id":"price_1QPt67DrtegwEnl3G95UWYBz",
     "description": "Connectetion speed"
     },{
 "product_name":"Quaterly",
+ "price": "$19.99",
 "product_id":"prod_RIU7SWP0z8tdWv",
 "price_id":"price_1QPt99DrtegwEnl3i67bcdyp",
 "description": "No adds, connectetion speed"
 },{
     "product_name":"Yearly",
+    "price": "$49.99",
     "product_id":"prod_RIU8JMDzPDj3Yc",
     "price_id":"price_1QPtAZDrtegwEnl3VG60SmQf",
     "description": "No adds, connectetion speed"
